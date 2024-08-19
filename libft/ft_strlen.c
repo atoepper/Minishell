a6fd5cb4e0cb946c9ab2e90f022c0420f188882d
jweingar <jweingar@student.42wolfsburg.de>
2024-08-19 11:14:16 +0200

    change str == NULL

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atoepper <atoepper@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jweingar <jweingar@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 14:53:26 by atoepper          #+#    #+#             */
-/*   Updated: 2024/01/30 13:00:32 by atoepper         ###   ########.fr       */
+/*   Updated: 2024/08/19 11:06:21 by jweingar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ size_t	ft_strlen(char *str)
 	size_t	count;
 
 	count = 0;
-	while (*str)
+	while (str != NULL && *str)
 	{
 		str++;
 		count++;
