f0da68152d5385e0afdd622d4e7bbc3fda421533
jweingar <jweingar@student.42wolfsburg.de>
2024-08-26 15:42:23 +0200

    change env to envp

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -3,20 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atoepper <atoepper@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jweingar <jweingar@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 16:35:06 by atoepper          #+#    #+#             */
-/*   Updated: 2024/08/26 11:18:28 by atoepper         ###   ########.fr       */
+/*   Updated: 2024/08/26 15:41:12 by jweingar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incl/minishell.h"
 
-int	ft_exit(char **argv)
+int	ft_exit(char **argv, char **envp)
 {
 	int	exitvalue;
 
-	(void)argv;
+	((void)argv, (void)envp);
 	exitvalue = 0;
 	exit(exitvalue);
 }
--- srcs/execution/execute.c
+++ srcs/execution/execute.c
@@ -6,18 +6,18 @@
 /*   By: jweingar <jweingar@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 12:09:29 by atoepper          #+#    #+#             */
-/*   Updated: 2024/08/20 13:53:36 by jweingar         ###   ########.fr       */
+/*   Updated: 2024/08/26 15:30:46 by jweingar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incl/minishell.h"
 
-int	exec_function(char **argv)
+int	exec_function(char **argv, char **env)
 {
 	if (argv == NULL)
 		return (0);
-	if (exec_builtin(argv))
-		if (exec_external(argv))
+	if (exec_builtin(argv, env))
+		if (exec_external(argv, env))
 			printf("command not found: %s\n", argv[0]);
 	return (0);
 }
@@ -38,4 +38,44 @@ char	*read_fd_to_str(int fd)
 			return (NULL);
 	}
 	return (str);
+}
+
+int	pipe_function(char *argvs[2][6], int in_fd)
+{
+	int		pipefd[2];
+	pid_t	pid;
+	int		exit_status;
+
+	if (argvs[0][0] == NULL)
+		return (0);
+	if (pipe(pipefd) == -1)
+		return (perror("pipe"), 1);
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		return (EXIT_FAILURE);
+	}
+	if (pid == 0)
+	{
+		if (in_fd != 0)
+		{
+			dup2(in_fd, 0);
+			close(in_fd);
+		}
+		if (argvs[1][0] != NULL)
+			dup2(pipefd[1], 1);
+		close(pipefd[0]);
+		exec_function(argvs[0]);
+		exit(EXIT_SUCCESS);
+	}
+	else if (pid != 0)
+	{
+		wait(&exit_status);
+		close(pipefd[1]);
+		if (in_fd != 0)
+			close(in_fd);
+		pipe_function(argvs + 1, pipefd[0]);
+	}
+	return (0);
 }
