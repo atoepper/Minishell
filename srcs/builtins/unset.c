b9f3a28904161fdf10e31295f0c2cfe72fdad6ba
jweingar <jweingar@student.42wolfsburg.de>
2024-08-27 11:37:35 +0200

    new

--- srcs/builtins/unset.c
+++ srcs/builtins/unset.c
@@ -6,14 +6,26 @@
 /*   By: jweingar <jweingar@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 15:53:41 by jweingar          #+#    #+#             */
-/*   Updated: 2024/08/27 10:01:08 by jweingar         ###   ########.fr       */
+/*   Updated: 2024/08/27 11:37:00 by jweingar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incl/minishell.h"
 
-int ft_unset(char **argv, char **envp)
+int	ft_unset(char **argv, t_env *envlst)
 {
-	((void)argv, (void)envp);
+	int	i;
+
+	i = 1;
+	if (argv == NULL)
+		return (1);
+	if (argv[1] == NULL)
+		return (printf("unset: not enough arguments\n"), 1);
+	while (argv[i] != NULL)
+	{
+		if (ft_find_value_by_key(envlst, argv[i]) != NULL)
+			ft_remove_env(&envlst, argv[i]);
+		i++;
+	}
 	return (0);
 }
