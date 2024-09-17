09a3b4322dbe99a4696791e29344d2b889ec1578
jweingar <jweingar@student.42wolfsburg.de>
2024-09-17 10:53:36 +0200

    edit errors

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: jweingar <jweingar@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 16:35:06 by atoepper          #+#    #+#             */
-/*   Updated: 2024/09/17 10:42:25 by jweingar         ###   ########.fr       */
+/*   Updated: 2024/09/17 10:47:54 by jweingar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,11 @@ int	ft_exit(char **argv, t_shell *mshell)
 	(void)mshell;
 	exitvalue = 0;
 	if (argv[1] != NULL)
+	{
 		if (argv[2] != NULL)
 			return (perror("exit: too many arguments"), 1);
 		exitvalue = ft_atoi(argv[1]);
+	}
 	mshell->is_exit_prog = TRUE;
 	mshell->exit_prog_val = exitvalue;
 	return (0);
