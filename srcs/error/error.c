75c545eb3ae3c8ffb759170a42f9c7aab66d630c
Alexander Toepper <atoepper@c2r9s4.42wolfsburg.de>
2024-09-17 10:09:44 +0200

    added printerror2file to error.c

--- srcs/error/error.c
+++ srcs/error/error.c
@@ -6,7 +6,7 @@
 /*   By: atoepper <atoepper@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 15:53:14 by atoepper          #+#    #+#             */
-/*   Updated: 2024/09/04 10:20:58 by atoepper         ###   ########.fr       */
+/*   Updated: 2024/09/17 10:07:27 by atoepper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,14 @@ void	ft_set_error(t_shell *mshell, int errno, char *msg)
 {
 	mshell->error = errno;
 	ft_putstr_fd(msg, 2);
+}
+
+void	print2errorfile(char *str)
+{
+	int	fd_output;
+	int	write_return;
+
+	fd_output = open("errorfile.txt", O_WRONLY | O_CREAT | O_APPEND, 0644);
+	write_return = write(fd_output, str, ft_strlen(str));
+	write_return++;
 }
