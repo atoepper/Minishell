a8b9b73123ffb041ec19bc363dcfa7e9e63c2c26
jweingar <jweingar@student.42wolfsburg.de>
2024-10-07 12:12:38 +0200

    change errorhandling

--- srcs/builtins/pwd.c
+++ srcs/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: jweingar <jweingar@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 16:51:47 by atoepper          #+#    #+#             */
-/*   Updated: 2024/09/17 13:17:36 by jweingar         ###   ########.fr       */
+/*   Updated: 2024/10/07 10:47:01 by jweingar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_pwd(char **argv, t_shell *mshell, int fd)
 
 	((void)argv, (void)mshell);
 	if (argv[1] != NULL)
-		return (perror("pwd: too many arguments"), 1);
+		return (ft_set_error(mshell, 1, "pwd: too many arguments\n"), 1);
 	pwd = NULL;
 	pwd = getcwd(pwd, 0);
 	if (!pwd)
