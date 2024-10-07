2bc0384686074d7db803485aaee67c8ae40d716e
jweingar <jweingar@student.42wolfsburg.de>
2024-10-07 12:14:42 +0200

    minimize funktoin

--- srcs/builtins/export.c
+++ srcs/builtins/export.c
@@ -6,41 +6,68 @@
 /*   By: jweingar <jweingar@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 15:53:41 by jweingar          #+#    #+#             */
-/*   Updated: 2024/09/17 15:23:23 by jweingar         ###   ########.fr       */
+/*   Updated: 2024/10/07 12:06:56 by jweingar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incl/minishell.h"
 
+int	print_env_with_empty(char **argv, t_shell *mshell, int fd)
+{
+	t_env	*envlst;
+	int		return_val;
+
+	(void)fd;
+	if (argv[1] != NULL)
+		return (ft_putstr_fd("env: too many arguments\n", 2), 1);
+	envlst = mshell->envlst;
+	if (envlst == NULL)
+		return (1);
+	while (envlst != NULL)
+	{
+			return_val = write(fd, envlst->key, ft_strlen(envlst->key));
+			return_val = write(fd, "=", 1);
+			return_val = write(fd, envlst->value, ft_strlen(envlst->value));
+			return_val = write(fd, "\n", 1);
+			envlst = envlst->next;
+			return_val++;
+	}
+	return (0);
+}
+
+void	change_or_create(char *arg, char *key, t_shell *mshell)
+{
+	if (ft_find_value_by_key(mshell->envlst, key) != NULL)
+	{
+		ft_change_envvalue(mshell->envlst, key, ft_getvalue(arg));
+		free(key);
+	}
+	else
+		ft_add_env(&(mshell->envlst), ft_new_env(key,
+				ft_getvalue(arg)));
+}
+
 int	ft_export(char **argv, t_shell *mshell, int fd)
 {
 	int		i;
 	char	*key;
-	char	*value;
 
+	if (argv == NULL)
+		return (1);
 	if (argv[1] == NULL)
-	{
-		ft_env(argv, mshell, fd);
-		return (0);
-	}
-	i = 1;
-	while (argv[i] != NULL)
+		return (print_env_with_empty(argv, mshell, fd), 0);
+	i = 0;
+	while (argv[++i] != NULL)
 	{
 		if (ft_strchr(argv[i], '=') != NULL)
 		{
 			key = ft_getkeyword(argv[i]);
-			value = ft_getvalue(argv[i]);
-			if (key == NULL || value == NULL)
+			if (key == NULL)
 				return (1);
-			if (ft_find_value_by_key(mshell->envlst, key) != NULL)
-			{
-				ft_change_envvalue(mshell->envlst, key, value);
-				free(key);
-			}
-			else
-				ft_add_env(&(mshell->envlst), ft_new_env(key, value));
+			change_or_create(argv[i], key, mshell);
 		}
-		i++;
+		else if (ft_find_value_by_key(mshell->envlst, argv[i]) == NULL)
+			ft_add_env(&(mshell->envlst), ft_new_env(ft_strdup(argv[i]), ft_strdup("''")));
 	}
 	return (0);
 }
