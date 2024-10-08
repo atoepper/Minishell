0e1fdecf768626fee879197e0830a3febf930e97
jweingar <jweingar@student.42wolfsburg.de>
2024-10-08 12:48:18 +0200

    add break when readline == NULL

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jweingar <jweingar@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 14:22:17 by atoepper          #+#    #+#             */
-/*   Updated: 2024/09/23 16:40:39 by jweingar         ###   ########.fr       */
+/*   Updated: 2024/10/08 12:47:58 by jweingar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,18 @@
 int	main(int argc, char **argv, char **envp)
 {
 	t_shell	mshell;
+
 	((void)argc, (void)argv);
 	init_shell(&mshell, envp);
 	while (!mshell.is_exit_prog)
 	{
 		mshell.line = readline(mshell.prompt);
 		if (!mshell.line)
-			return (free(mshell.line), 0);
+			break ;
 		add_history(mshell.line);
 		ft_tokenize(&mshell);
 		mshell.ast = parse_program(&mshell);
 		execute_programm(&mshell);
-		if (is_output_without_nl(&mshell))
-			printf("%%\n");
 		ft_renewshell(&mshell);
 	}
 	clear_garbage(&mshell);
