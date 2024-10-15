3327ca0fb3260ae8f979ca62f9e64e475850469c
jweingar <jweingar@student.42wolfsburg.de>
2024-10-15 10:34:42 +0200

    edit s1 == NULL

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atoepper <atoepper@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jweingar <jweingar@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 15:05:36 by atoepper          #+#    #+#             */
-/*   Updated: 2024/01/30 12:08:04 by atoepper         ###   ########.fr       */
+/*   Updated: 2024/10/15 10:34:19 by jweingar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ char	*ft_strdup(const char *s1)
 {
 	char	*dupl;
 
+	if (s1 == NULL)
+		return (NULL);
 	dupl = (char *)malloc(ft_strlen((char *)s1) + 1);
 	if (dupl == NULL)
 		return (NULL);
