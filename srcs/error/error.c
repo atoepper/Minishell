5e709c34399430e0ca8f36e67af6e8c39bcb0ce3
atoepper <atoepper@student.42wolfsburg.de>
2024-10-16 12:32:13 +0200

    updated error messages

--- srcs/error/error.c
+++ srcs/error/error.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jweingar <jweingar@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*   By: atoepper <atoepper@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 15:53:14 by atoepper          #+#    #+#             */
-/*   Updated: 2024/10/15 10:45:06 by jweingar         ###   ########.fr       */
+/*   Updated: 2024/10/16 12:29:29 by atoepper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,10 +45,10 @@ void	ft_process_error(t_shell *mshell)
 		ft_putstr_fd("minishell: no such directory or file\n", 2);
 	else if (err_type == EXIT_ARG)
 		ft_putstr_fd("exit: too many arguments\n", 2);
+	else if (err_type == CD_ARG)
+		ft_putstr_fd("cd: too many arguments\n", 2);
 	else if (err_type == EXIT_ARG_NUM)
 		ft_putstr_fd("exit: numeric argument required\n", 2);
-	else if (err_type == PWD_ARG)
-		ft_putstr_fd("pwd: too many arguments\n", 2);
 }
 
 void	print2errorfile(char *str)
